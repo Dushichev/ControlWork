05821fc99589354a6b29b610c29147c6601580f5
Dushichev <108294464+Dushichev@users.noreply.github.com>
2022-09-04 16:48:33 +0300

    поправки

--- ControlTask/Program.cs
+++ ControlTask/Program.cs
@@ -5,19 +5,26 @@
  лучше обойтись исключительно массивами. */
 
 string[] arr1 = { "334", "folok", "rokki", "23", "Foo" };
-string[] arr2 = new string[arr1.Length];
+int size = 0;
 int count = 0;
 
 for (int i = 0; i < arr1.Length; i++)
 {
     if (arr1[i].Length <= 3)
     {
-        count++;
-
-        arr2[count] = arr1[i];
-
-        Console.Write(arr2[count] + " ");
+        size++;
     }
-
 }
 
+string[] arr2 = new string[size];
+
+
+for (int i = 0; i < arr1.Length; i++)
+{
+    if (arr1[i].Length <= 3)
+    {
+        arr2[count] = arr1[i];
+        Console.Write(arr2[count] + " ");
+
+    }
+}
